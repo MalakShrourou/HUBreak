dcfb64be2778480afe554376f05f77fd1e3a9176
MalakShrourou <malakfikri36@gmail.com>
2023-04-30 00:44:39 +0300

    new file:   1star.png 	new file:   2stars.png 	new file:   3stars.png 	new file:   4stars.png 	new file:   5stars.png 	modified:   FastFood.png 	modified:   home.html 	modified:   signup.php

--- signup.php
+++ signup.php
@@ -4,10 +4,13 @@ if (!$database = mysqli_connect("localhost", "root", "12345678"))
 if (!mysqli_select_db($database, "hubreak_db"))
     die("Sorry, could not find database.");
 extract($_POST);
-$query = "INSERT INTO user (name, email, pass) values ('$name' , '$email', '$pass')";
-if (!mysqli_query($database, $query))
-    header("Location:Signup_failed.html");
-else
-    header("Location:Account_created.html");
+if (preg_match('/^([0-9A-Za-z_$]{8})$/', $pass)) {
+    $query = "INSERT INTO user (name, email, pass) values ('$name' , '$email', '$pass')";
+    if (!mysqli_query($database, $query))
+        header("Location:Signup_failed.html");
+    else
+        header("Location:Account_created.html");
+} else
+    die("try another pass");
 mysqli_close($database);
 ?>
