9c8706be5f4e36e341c3bfc35f79a30bb30bf82a
MalakShrourou <malakfikri36@gmail.com>
2023-05-06 22:42:58 +0300

    new file:   addtocart.php 	modified:   alteb.html 	modified:   cart.php 	modified:   espressos.html 	modified:   gharby.html 	modified:   home.html 	modified:   sharqy.html 	modified:   village.html 	modified:   zaza.html

---
+++ addtocart.php
@@ -0,0 +1,12 @@
+<?php
+if (!$database = mysqli_connect("localhost", "root", "12345678"))
+    die("Sorry, could not connect to the server.");
+if
+(!mysqli_select_db($database, "hubreak_db"))
+    die("Sorry, could not find database.");
+extract($_POST);
+$query = "INSERT INTO cart (ID, quantity, price) VALUES ($ID, $quantity, $price)";
+mysqli_query($database, $query);
+mysqli_close($database);
+header("location:javascript://history.go(-1)"); 
+?>
--- cart.php
+++ cart.php
@@ -1,16 +1,14 @@
 <!Doctype html>
+
 <html>
 
 <head>
-    <style>
-    body {
-        color: rgb(124, 17, 17);
-        font-family: 'Courier New', Courier, monospace;
+    <title>HU Break</title>
+    <meta charset="UTF-8">
+    <style type="text/css">
+    * {
         margin: 0;
-    }
-
-    table {
-        margin: 2%;
+        font-family: 'Courier New', Courier, monospace;
     }
 
     nav {
@@ -37,11 +35,10 @@
         text-align: center;
         padding: 18px;
         text-decoration: none;
-        font-family: 'Courier New', Courier, monospace;
     }
 
     nav li a:hover {
-        background-color: #ff9208;
+        background-color: #fda025;
     }
 
     @media screen and (max-width: 600px) {
@@ -59,60 +56,67 @@
         h1 {
             font-size: 24px;
         }
-    }
+
+        table {
+            width: 50%;
+            margin: 20%;
+        }
+
+        footer {
+            background-color: #f18b05;
+            color: white;
+            font-weight: bold;
+            text-align: center;
+            padding: 15px;
+        }
     </style>
 </head>
 
 <body>
     <nav>
         <ul>
-            <li class="cart"><a href="cart.php">سلة المشترايات</a></li>
+            <li class="cart"><a href="cart.php">سلة المشتريات</a></li>
             <li><a href="#">العروض</a></li>
             <li><a href="#">من نحن</a></li>
             <li><a href="#">تواصل معنا</a></li>
             <li><a href="#">مواقع المطاعم</a></li>
             <li><a href="home.html">الصفحة الرئيسية</a></li>
         </ul>
-        <img src="logo.jpg" height="50px">
+        <img src="logo.jpg" height="48px">
     </nav>
-    <h1>The Cart</h1>
-
-
     <?php
     if (!$database = mysqli_connect("localhost", "root", "12345678"))
         die("Sorry, could not connect to the server.");
     if (!mysqli_select_db($database, "hubreak_db"))
         die("Sorry, could not find database.");
     extract($_POST);
-    $query = "select cart.ID, p_name,p_price,quantity from products,cart where products.ID = cart.ID";
+    $query = "select ID,name,quantity, price from espresso,cart where espresso.ID = cart.ID";
     $result = mysqli_query($database, $query);
     print("<table border='1px'>");
     print("<thead>");
-    print("<th>Id</th>");
-    print("<th>Products</th>");
-    print("<th>Price</th>");
+    print("<th>ID</th>");
+    print("<th>Proudct</th>");
     print("<th>Quantity</th>");
+    print("<th>Price</th>");
     print("</thead>");
     $total = 0;
-    print("<form mathod='post' action='clearcart.php'>");
     while ($row = mysqli_fetch_row($result)) {
         print("<tr>");
-        print("<form mathod='post' action='deletecart.php'>");
         foreach ($row as $value) {
             print("<td>$value</td>");
         }
         $total = $total + $row[2] * $row[3];
         print("<input type='hidden' name='ID' value='$row[0]'>");
-        print("<td><input type='submit' value='Delete'></td>");
-        print("</form>");
         print("</tr>");
     }
     print("<tfoot><th colspan='3'>Total</th><th>$total</th></tfoot>");
     print("</table>");
-    print("<input type='submit' value='Place order'></form>");
     mysqli_close($database);
     ?>
-
+    <footer>
+        <p>Developed By</p>
+        <p>HU Break Team &copy;</p>
+    </footer>
 </body>
 
 </html>
