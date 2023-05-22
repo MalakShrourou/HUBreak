84662af1dd447a441cbcdd12629e0510fbaef21e
MalakShrourou <malakfikri36@gmail.com>
2023-05-22 14:36:24 +0300

    Merge branch 'master' of https://github.com/MalakShrourou/HUBreak

--- Signup.php
+++ Signup.php
@@ -27,143 +27,139 @@ mysqli_close($database);
 
 <head>
     <style>
-    @import url(https://fonts.googleapis.com/css?family=Roboto:300);
+        @import url(https://fonts.googleapis.com/css?family=Roboto:300);
 
-    * {
-        font-family: 'Times New Roman', Times, serif;
-    }
+        * {
+            font-family: 'Times New Roman', Times, serif;
+        }
 
-    .login-page {
-        width: 460px;
-        padding: 8% 0 0;
-        margin: auto;
-    }
+        body {
+            background-image: url("cover.jpg");
+        }
 
-    .form {
-        position: relative;
-        z-index: 1;
-        background: #FFFFFF;
-        max-width: 360px;
-        margin: 0 auto 100px;
-        padding: 45px;
-        text-align: center;
-        box-shadow: -30px 30px 20px rgba(0, 0, 0, 0.3);
-    }
+        img {
+            margin: 5% auto 0 30%;
+        }
 
-    .form input {
-        font-family: "Roboto", sans-serif;
-        outline: 0;
-        background: #f2f2f2;
-        width: 100%;
-        border: 0;
-        margin: 0 0 15px;
-        padding: 15px;
-        box-sizing: border-box;
-        font-size: 14px;
-    }
+        .login-page {
+            width: 460px;
+            padding: 10px;
+            margin: 0 auto 0 30%;
+        }
 
-    .form button {
-        font-family: "Roboto", sans-serif;
-        outline: 0;
-        background: orange;
-        width: 100%;
-        border: 0;
-        padding: 15px;
-        color: #FFFFFF;
-        font-size: 14px;
-        -webkit-transition: all 0.3 ease;
-        transition: all 0.3 ease;
-        cursor: pointer;
-    }
+        .form {
+            position: relative;
+            background: #FFFFFF;
+            max-width: 360px;
+            padding: 45px;
+            text-align: center;
+            box-shadow: -30px 30px 20px rgba(0, 0, 0, 0.3);
+            margin-top: -10px;
+        }
 
-    .form button:hover,
-    .form button:active,
-    .form button:focus {
-        background: #b88715;
-    }
+        .form input {
+            outline: 0;
+            background: #f2f2f2;
+            width: 90%;
+            border: 0;
+            margin: 0 0 15px;
+            padding: 15px;
+            box-sizing: border-box;
+            font-size: 14px;
+        }
 
-    .form .message {
-        margin: 15px 0 0;
-        color: #b3b3b3;
-        font-size: 12px;
-    }
+        .form button {
+            outline: 0;
+            background: orange;
+            width: 50%;
+            border: 0;
+            padding: 15px;
+            color: #FFFFFF;
+            font-size: 14px;
+            -webkit-transition: all 0.3 ease;
+            transition: all 0.3 ease;
+            cursor: pointer;
+        }
 
-    .form .message a {
-        color: orange;
-        text-decoration: none;
-    }
+        .form button:hover,
+        .form button:active,
+        .form button:focus {
+            background: #b88715;
+        }
 
-    .form .register-form {
-        display: none;
-    }
+        .form .message {
+            margin: 15px 0 0;
+            color: #b3b3b3;
+            font-size: 12px;
+        }
 
-    .container {
-        position: relative;
-        z-index: 1;
-        max-width: 300px;
-        margin: 0 auto;
-    }
+        .form .message a {
+            color: orange;
+            text-decoration: none;
+        }
 
-    .container:before,
-    .container:after {
-        content: "";
-        display: block;
-        clear: both;
-    }
+        .form .register-form {
+            display: none;
+        }
 
-    .container .info {
-        margin: 50px auto;
-        text-align: center;
-    }
+        .container {
+            position: relative;
+            z-index: 1;
+            max-width: 300px;
+            margin: 0 auto;
+        }
 
-    .container .info h1 {
-        margin: 0 0 15px;
-        padding: 0;
-        font-size: 36px;
-        font-weight: 300;
-        color: #1a1a1a;
-    }
+        .container:before,
+        .container:after {
+            content: "";
+            display: block;
+            clear: both;
+        }
 
-    .container .info span {
-        color: #4d4d4d;
-        font-size: 12px;
-    }
+        .container .info {
+            margin: 50px auto;
+            text-align: center;
+        }
 
-    .container .info span a {
-        color: #000000;
-        text-decoration: none;
-    }
+        .container .info h1 {
+            margin: 0 0 15px;
+            padding: 0;
+            font-size: 36px;
+            font-weight: 300;
+            color: #1a1a1a;
+        }
 
-    .container .info span .fa {
-        color: #EF3B3A;
-    }
+        .container .info span {
+            color: #4d4d4d;
+            font-size: 12px;
+        }
 
-    body {
-        background-image: url("cover.jpg");
-        font-family: "Roboto", sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
+        .container .info span a {
+            color: #000000;
+            text-decoration: none;
+        }
 
-    .error {
-        color: red;
-        font-family: "Roboto", sans-serif;
-        font-weight: bold;
-        text-align: center;
-    }
+        .container .info span .fa {
+            color: #EF3B3A;
+        }
 
-    .success {
-        color: lawngreen;
-        font-family: "Roboto", sans-serif;
-        font-weight: bold;
-        text-align: center;
-    }
+        .error {
+            color: red;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        .success {
+            color: lawngreen;
+            font-weight: bold;
+            text-align: center;
+        }
     </style>
 </head>
 
 <body>
+    <img src="welcome3.png" height="150" width="500">
     <div class="login-page">
-        <img src="welcome3.png" height="150" width="500">
         <div class="form">
             <form class="login-form" method="POST">
                 <input type="text" name="name" placeholder="Name" />
